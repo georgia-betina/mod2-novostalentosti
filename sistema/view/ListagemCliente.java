8e1a97a62da896548c1557b54939b86b2c1ea256
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-27 15:22:29 -0300

    aula

--- sistema/view/ListagemCliente.java
+++ sistema/view/ListagemCliente.java
@@ -1,18 +1,42 @@
 package sistema.view;
 
+import java.awt.Color;
+
+import javax.management.loading.PrivateClassLoader;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ListagemCliente extends JFrame {
+
+    /* === PAINEL === */
+    private JPanel jpnCentro = new JPanel();
+    private JPanel jpnSul = new JPanel();
+
+    /* ==== BOTÃO === */
+    private JButton btnNovo = new JButton("Novo");
+    private JButton btnAlterar = new JButton("Alterar");
+    private JButton btnExcluir = new JButton("Excluir");
+
     public ListagemCliente() {
+        // PAINEL
         JPanel painel = new JPanel();
         add(painel,"Center");
-        setSize(400,400);
+        setSize(500,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        criaPaineis(); // chama o método
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new ListagemCliente();
+    private void criaPaineis(){
+        add(jpnCentro, "Center");
+        add(jpnSul, "South");
+        /*
+        * jpnCentro.setBackground(Color.MAGENTA);
+        * jpnSul.setBackground(Color.YELLOW);
+        */
+        jpnSul.add(btnNovo);
+        jpnSul.add(btnAlterar);
+        jpnSul.add(btnExcluir);
     }
 }
