991c6a5c5f877aa59d80794894e8d224f6f9ef56
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-28 17:29:06 -0300

    aula

--- exercicios/introducao/App4.java
+++ exercicios/introducao/App4.java
@@ -1,7 +1,9 @@
 package exercicios.introducao;
 
+import exercicios.introducao.exercicio1.Livro;
+
 public class App4 {
     public static void main(String[] args) {
-        
+        Livro novoLivro = new Livro();
     }
 }
