e1249c591a188a969e9aaa91c3cbbb94cdcc418c
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-04-28 15:09:50 -0300

    aula

--- sistema/controller/ListagemClienteController.java
+++ sistema/controller/ListagemClienteController.java
@@ -3,6 +3,7 @@ package sistema.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 public class ListagemClienteController implements ActionListener {
@@ -10,6 +11,20 @@ public class ListagemClienteController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
+        JButton botaoClicado = (JButton)e.getSource();
+        switch (botaoClicado.getName()) {
+            case "btnNovo":
+            JOptionPane.showMessageDialog(null, "BOTAO NOVO");
+            break;
+            case "btnAlterar":
+            JOptionPane.showMessageDialog(null, "BOTAO ALTERAR");
+            break;
+            case "btnExcluir":
+            JOptionPane.showMessageDialog(null, "BOTAO EXCLUIR");
+            break;
+            default:
+            break;
+        }
         JOptionPane.showMessageDialog(null, "Olá, mundo.");
     }
     
