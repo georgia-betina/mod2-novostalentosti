519b439ebd867108543533deeb89af39f6941535
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-05-03 14:22:10 -0300

    aula

---
+++ sistema/model/TabelaClienteModel.java
@@ -0,0 +1,26 @@
+package sistema.model;
+
+import javax.swing.table.AbstractTableModel;
+
+public class TabelaClienteModel extends AbstractTableModel {
+    // Classe abstrata: junção de uma classe com uma interface
+
+    @Override
+    public int getRowCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
