65aff29916898556397f0e8ebe502fad92fc8498
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-05-05 16:03:43 -0300

    exercises

--- sistema/App3.java
+++ sistema/App3.java
@@ -1,9 +1,9 @@
 package sistema;
 
-import sistema.view.ListagemCliente;
+// import sistema.view.ListagemCliente;
 
 public class App3 {
     public static void main(String[] args) {
-        ListagemCliente listagem = new ListagemCliente();
+        //ListagemCliente listagem = new ListagemCliente();
     }
 }
