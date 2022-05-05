3c8987a40e6ef306c2cbe3c2b6ea41552d2f85a7
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-05-05 15:58:02 -0300

    aula

--- exercicios/introducao/exercicio1/Livro.java
+++ exercicios/introducao/exercicio1/Livro.java
@@ -1,5 +1,7 @@
 package exercicios.introducao.exercicio1;
 
+//import java.text.SimpleDateFormat;
+
 public class Livro {
     private String titulo;
     private Float valor;
@@ -42,17 +44,18 @@ public class Livro {
         /*return "Livro \"" + titulo + "\"\n" + "Autor(a): " + getEscritor().getNome() + " | Data de nascimento: " + getEscritor().getDataNascimento() + "\nEstoque: " + esgotado + "\n" + "Valor: R$ "
                 + valor;*/
         StringBuilder montadorString = new StringBuilder();
+        //SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
 
-            montadorString.append("Título: ");
-            montadorString.append(getTitulo());
-            montadorString.append("Valor: R$ ");
-            montadorString.append(getValor());
-            montadorString.append("Esgotado: ");
-            montadorString.append(getEsgotado());
-            montadorString.append("Autor(a): ");
-            montadorString.append(escritor.getNome());
-            montadorString.append("Data de nascimento: ");
-            montadorString.append(escritor.getDataNascimento());
+            montadorString.append(String.format("\n\nTítulo: %s", getTitulo()));
+            //montadorString.append(getTitulo());
+            montadorString.append(String.format("\nValor: R$ %.2f", getValor()));
+            //montadorString.append(getValor());
+            montadorString.append(String.format("\nEsgotado: %s", getEsgotado()));
+            //montadorString.append(getEsgotado());
+            montadorString.append(String.format("\nAutor(a): %s", getEscritor()));
+            //montadorString.append(escritor.getNome());
+            montadorString.append(String.format("\nData de nascimento: %s", getEscritor().getDataNascimento()));
+            //montadorString.append(escritor.getDataNascimento());
             
         return montadorString.toString();
     }
