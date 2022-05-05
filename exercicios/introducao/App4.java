ff4e8ebf3b12344fcfa84085d167304828915002
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-05-05 15:42:34 -0300

    exercises

--- exercicios/introducao/App4.java
+++ exercicios/introducao/App4.java
@@ -1,6 +1,7 @@
 package exercicios.introducao;
 
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 //import java.util.GregorianCalendar;
 
 import exercicios.introducao.exercicio1.Autor;
@@ -8,21 +9,31 @@ import exercicios.introducao.exercicio1.Livro;
 
 public class App4 {
     public static void main(String[] args) {
+        SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
 
-        Autor autorLivro1 = new Autor("Deitel", new Date(1945,01,01));
+        Autor autorLivro1;
+        try {
+            autorLivro1 = new Autor("Deitel", dataFormatada.parse("01/01/1945"));
+            Livro livro1 = new Livro("Java como Programar");
+            livro1.setValor(349f);
+            livro1.setEsgotado(false);
+            livro1.setEscritor(autorLivro1);
+            System.out.printf(livro1.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         //GregorianCalendar
-        Livro livro1 = new Livro("Java como Programar");
-        livro1.setValor(349f);
-        livro1.setEsgotado(false);
-        livro1.setEscritor(autorLivro1);
-
-        Autor autorLivro2 = new Autor("Grady Booch", new Date());
-        Livro livro2 = new Livro("Java 2 Ensino Didático");
-        livro2.setValor(126f);
-        livro2.setEsgotado(true);
-        livro2.setEscritor(autorLivro2);
-
-        System.out.printf(livro1.toString());
-        System.out.printf(livro2.toString());
+        
+        Autor autorLivro2;
+        try {
+            autorLivro2 = new Autor("Grady Booch", dataFormatada.parse("27/02/1955"));
+            Livro livro2 = new Livro("Java 2 Ensino Didático");
+            livro2.setValor(126f);
+            livro2.setEsgotado(true);
+            livro2.setEscritor(autorLivro2);
+            System.out.printf(livro2.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
