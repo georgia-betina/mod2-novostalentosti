e6ab685e802e79c5b4f7c0bf8c838c5c559727d2
GEÓRGIA BETINA HARITSCH <georgia.haritsch@univille.br>
2022-06-09 16:06:25 -0300

    testes executados

--- testesdeunidade/exercicios/src/main/java/br/univille/ImpostoDeRenda.java
+++ testesdeunidade/exercicios/src/main/java/br/univille/ImpostoDeRenda.java
@@ -5,6 +5,21 @@ import java.util.ArrayList;
 public class ImpostoDeRenda {
     private float valorImptoRetidonaFonte;
     private ArrayList<Rendimento> listaRendimentos = new ArrayList<>();
+    private float calculaRendimento() {
+        float somaRendimentos = 0;
+        for (Rendimento umRendimento : listaRendimentos) {
+            somaRendimentos += umRendimento.getValor();
+        }
+        final float rendimentosPorMes = somaRendimentos / 12;
+        return rendimentosPorMes;
+    }
+
+    private float aplicaIRPF(float imposto) {
+        float aplicaIRPF = 0;
+        aplicaIRPF = calculaRendimento() * imposto;
+        aplicaIRPF = calculaRendimento() - aplicaIRPF - valorImptoRetidonaFonte;
+        return aplicaIRPF;
+    }
 
     public float getValorImptoRetidonaFonte() {
         return valorImptoRetidonaFonte;
@@ -21,6 +36,16 @@ public class ImpostoDeRenda {
     }
 
     public float calculaValorIRPF() {
-        return 0f;
+        if (calculaRendimento() >= 1903.99 && calculaRendimento() <= 2826.65) {
+            return aplicaIRPF(0.075f);
+        } else if (calculaRendimento() >= 2826.66 && calculaRendimento() <= 3751.05) {
+            return aplicaIRPF(0.15f);
+        } else if (calculaRendimento() >= 3751.06 && calculaRendimento() <= 4664.68) {
+            return aplicaIRPF(0.255f);
+        } else if (calculaRendimento() > 4664.68) {
+            return aplicaIRPF(0.275f);
+        } else {
+            return 0f;
+        }
     }
 }
